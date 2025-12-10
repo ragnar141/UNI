d83dd368d876438492ff20816d7f47fd98866cf9
Amir Yakhin <amir.yakhin94@google.com>
2025-12-09 18:20:27 -0600

    changed vite.config.js to set base to '/'

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 
 export default defineConfig({
-  base: "/UNI/",    // <-- replace with your repo name
+  base: '/',    // <-- replace with your repo name
   plugins: [react()],
   build: { outDir: "dist" }
 });
